c98dc26634beafcaccca0bfb184beec053b07553
Maksim <shchelkonogov@mail.ru>
2022-01-20 16:37:47 +0300

    Сделал корректный путь с * не работают html страницы

--- src/main/java/ru/tn/server/rest/MyApplication.java
+++ src/main/java/ru/tn/server/rest/MyApplication.java
@@ -5,13 +5,6 @@ import javax.ws.rs.core.Application;
 import java.util.HashSet;
 import java.util.Set;
 
-@ApplicationPath("/")
+@ApplicationPath("/rs")
 public class MyApplication extends Application {
-
-    @Override
-    public Set<Class<?>> getClasses() {
-        Set<Class<?>> items = new HashSet<>();
-        items.add(RestService.class);
-        return items;
-    }
 }
